2091cfad4c3b9c3c4b77d7887cdda40e3e1e495e
akbar gulyamov <kbrgulyamov@gmail.com>
2023-02-26 21:29:23 +0500

    generic logic add

--- dist/generic.js
+++ dist/generic.js
@@ -4,9 +4,14 @@ const cars2 = ["Ford", "Audi"];
 const promise = new Promise((resolve) => {
     setTimeout(() => {
         resolve(new Date().getHours());
-    }, 100);
+    }, 1000);
 });
 promise.then((data) => {
     alert(data.toFixed() + `${"this is hours"}`);
 });
+function mergeObjects(a, b) {
+    return Object.assign({}, a, b);
+}
+const merged = mergeObjects({ name: "Akbar" }, { age: 18 });
+console.log(merged.name, merged.age);
 //# sourceMappingURL=generic.js.map
--- src/generic.ts
+++ src/generic.ts
@@ -4,9 +4,24 @@ const cars2: Array<string> = ["Ford", "Audi"];
 const promise = new Promise<number>((resolve) => {
   setTimeout(() => {
     resolve(new Date().getHours());
-  }, 100);
+  }, 1000);
 });
 
 promise.then((data) => {
   alert(data.toFixed() + `${"this is hours"}`);
 });
+
+type T = {
+  name: string;
+};
+
+type R = {
+  age: number;
+};
+
+function mergeObjects<T, R>(a: T, b: R) {
+  return Object.assign({}, a, b);
+}
+
+const merged = mergeObjects({ name: "Akbar" }, { age: 18 });
+console.log(merged.name, merged.age);
